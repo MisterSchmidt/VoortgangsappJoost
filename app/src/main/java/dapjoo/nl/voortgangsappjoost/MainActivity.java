1c7c1ec980bb1bbe981e70ee8eb36b2addad043b
Joost van Dam <jv.dam@live.nl>
2020-01-07 15:36:45 +0100

    Je moet ergens beginnen, Hello world

---
+++ app/src/main/java/dapjoo/nl/voortgangsappjoost/MainActivity.java
@@ -0,0 +1,14 @@
+package dapjoo.nl.voortgangsappjoost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
