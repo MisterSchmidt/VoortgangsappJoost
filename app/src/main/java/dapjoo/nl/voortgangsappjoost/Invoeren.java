050ce3afa0b4150be806808518881936fef86f69
Joost van Dam <jv.dam@live.nl>
2020-01-09 14:51:39 +0100

    Save en load settings toegevoegd voor later gebruik

--- app/src/main/java/dapjoo/nl/voortgangsappjoost/Invoeren.java
+++ app/src/main/java/dapjoo/nl/voortgangsappjoost/Invoeren.java
@@ -1,11 +1,17 @@
 package dapjoo.nl.voortgangsappjoost;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.util.Log;
+
+
+import static dapjoo.nl.voortgangsappjoost.MainActivity.PREFS_NAME;
 
 public class Invoeren extends AppCompatActivity {
 
@@ -14,6 +20,14 @@ public class Invoeren extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invoeren);
 
+        //Ophalen Shared Preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0); //Find
+        boolean silent = settings.getBoolean("silentMode", false); // Read
+        Log.d(" Found this value :",""+ silent +"" ); // Use
+
+        loadSettings();
+
+        //Button om terug te gaan naar mainactivity
         Button button = (Button)findViewById(R.id.buttonToMain);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,8 +36,19 @@ public class Invoeren extends AppCompatActivity {
             }
         });
     }
-    public void openMain(){
+
+    public void openMain(){ //method om terug te gaan naar main
         Intent intent = new Intent (this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void loadSettings(){
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        String nieuweWaarde = settings.getString("setting", "Default");
+
+        //Textview om te kunnen kijken wat er gebeurd
+        TextView textView1 = (TextView)findViewById(R.id.textView1);
+        textView1.setText(nieuweWaarde);
+
     }
 }
--- app/src/main/java/dapjoo/nl/voortgangsappjoost/MainActivity.java
+++ app/src/main/java/dapjoo/nl/voortgangsappjoost/MainActivity.java
@@ -3,11 +3,19 @@ package dapjoo.nl.voortgangsappjoost;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String PREFS_NAME = "MijnSettings";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +23,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = (Button)findViewById(R.id.buttonToInvoeren);
+        Button saveButton = (Button)findViewById(R.id.saveSettings);
+
+        loadSettings();
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openInvoeren();
             }
         });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveSettings();
+            }
+        });
+
+
+
     }
 
     public void openInvoeren() {
         Intent intent = new Intent(this, Invoeren.class);
-        startActivity(intent);
+        startActivity(intent); // code om nieuwe activity met intent te laden
+    }
+
+    public void saveSettings(){
+        // Store a value named silentMode ( of type: boolean ) :
+        EditText editText = (EditText) findViewById(R.id.editField);
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit(); // EDIT MODE
+        editor.putString("setting", editText.getText().toString()); // WRITE STUFF
+        editor.commit(); // SAVE IT
+    }
+
+    public void loadSettings(){
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        String nieuweWaarde = settings.getString("setting", "Default");
+
+        EditText editText = (EditText) findViewById(R.id.editField);
+        editText.setText(nieuweWaarde);
     }
 }
